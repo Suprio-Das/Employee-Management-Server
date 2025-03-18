48a2353bf866d75e7f2fad430c8d831f4cc53aeb
Suprio-Das <priyamd507@gmail.com>
2025-03-18 10:55:37 +0600

    Server completedly setup and started successfully

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Server Dependency Installed!');
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+// Creating Express App
+const app = express();
+
+// Middlewares
+app.use(express());
+app.use(cors());
+
+// Testing Route
+app.get('/', (req, res) => {
+    res.send('Hola, Server is running!');
+})
+
+app.listen(PORT);
